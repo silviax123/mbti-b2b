cdd3c951deb6da0c46aa236f5353c50f72dee747
Silvia <sx0771@gmail.com>
2023-07-25 15:55:16 -0400

    finish calculation

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,72 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { Answer, Choice, Choices, Domain, Question, questionTypes } from "../types";
+import React, { useState } from "react";
+import { Choice, Domain, Question } from "../types";
 import QuestionCard from "./QuestionCard";
 import { Step, Steps, Wizard } from "react-albus";
 import ChoicesData from "../data/choices.json";
 import QuestionData from "../data/questions.json";
 
-
 const Quiz: React.FC = (props) => {
-  // const choicesData = ChoicesData as Choices;
   const questionsData = QuestionData as Question[];
-  const [currentStep, setCurrentStep] = useState(0);
-  const [currentQuestion, setCurrentQuestion] = useState<Question | undefined>();
-  const [choices, setChoices] = useState<Choice[]>([]);
-  const [answers, setAnswers] = useState<Choice[]>([]);
-  const [tempSum, setTempSum] = useState(0)
-  const [tempType, setTempType] = useState('E')
-  // const [sum, setSum] = useState({ 'E': 0, 'S': 0, 'T': 0, 'J':0 })
-
-
   const [sum, setSum] = useState<{ [key in Domain]: number }>({
-    'E': 0, 'S': 0, 'T': 0, 'J':0,
+    'E': 0, 'S': 0, 'T': 0, 'J':0
   });
 
-
-  const handleAnswer = (choice: Choice) => {
-    setChoices((prevChoices) => [...prevChoices, choice]);
-    setCurrentStep((prevStep) => prevStep + 1);
-    setCurrentQuestion(questionsData[currentStep + 1])
-
+  const handleAnswer = (choice: Choice, question: Question) => {
     const tempSum = choice.score;
-    const tempType = currentQuestion?.questionType
-    setSum((prevSum) => ({
-      ...prevSum,
-      tempType: prevSum[tempType] + tempSum
-    }))
+    const tempType = question?.questionType;
+    const newSum = {...sum} // by value (decomposition)
+    console.log('sum',sum)
+    console.log('newSum', newSum)
+    newSum[tempType] += tempSum
+
+    setSum(newSum)
     console.log("tempSum", tempSum);
     console.log("tempType", tempType);
   };
-
-
-
-  console.log("choices", choices); 
-  console.log("currentStep:", currentStep);
-  console.log("currentQuestion", currentQuestion);
-  // console.log("sum:", sum)
-
-
 
   return (
     <div>
       <Wizard>
         <Steps>
-          {questionsData.map((question, index) => (
+          {questionsData.map((question, index) => {
+            // console.log(question)
+            // console.log(index)
+            return (
             <Step
               id={`${index}`}
               render={({ next }) => (
                 <QuestionCard
                   question={question}
                   choices={ChoicesData[question.questionOrder]}
-                  // questionType={question.questionType}
                   onAnswer={(choice) => {
-                    handleAnswer(choice);
+                    handleAnswer(choice, question);
                     next();
                   }}
                 />
               )}
-            />
-          ))}
+            />)})
+          }
         </Steps>
       </Wizard>
       <div>
